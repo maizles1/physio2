69c87b8f632b017c95eaefe7115650a4d752c503
maizles1 <maizles1@gmail.com>
2026-01-07 13:11:33 +0200

    הוספת Google Tag Manager noscript ב-body

--- app/layout.tsx
+++ app/layout.tsx
@@ -237,6 +237,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-58XQH9KN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ErrorBoundary>
           <GoogleAnalytics />
           <PageTracking />
